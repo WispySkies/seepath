75381eb4bbe6b6077b15dcb4bbefd92b9a5780ac
WispySkies <arpolixgames@gmail.com>
2025-03-15 12:06:25 -0400

    Initial Commit

---
+++ seepath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  seepath
+//
+//  Created by Declan McCue on 3/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ seepath/seepathApp.swift
@@ -0,0 +1,17 @@
+//
+//  seepathApp.swift
+//  seepath
+//
+//  Created by Declan McCue on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct seepathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
